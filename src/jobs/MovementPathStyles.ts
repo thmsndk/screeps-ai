3d227f9b729fef2bb3973aa7a011bf332155bbfd
thmsn <thmsn.dk@gmail.com>
2019-05-27 03:27:20 +0200

    changed collection pathing color to yellow

--- src/jobs/MovementPathStyles.ts
+++ src/jobs/MovementPathStyles.ts
@@ -3,6 +3,6 @@ export const PathStyle = {
     Deposit: { opacity: 0.6, stroke: '#ffaa00' } as PolyStyle,
     Harvest: { opacity: 0.6, stroke: '#0000FF' } as PolyStyle,
     UpgradeController: { opacity: 0.6, stroke: '#ffffff' } as PolyStyle,
-    Collection: { opacity: 0.6, stroke: '#00FF00' } as PolyStyle,
+    Collection: { opacity: 0.6, stroke: '#FFFF00' } as PolyStyle,
     Construction: { opacity: 0.6, stroke: '#00FF00' } as PolyStyle
 }
