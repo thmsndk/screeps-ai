996cdf3c1f8d6dc8d32dead9585d46bedef8f475
thmsn <thmsn.dk@gmail.com>
2019-05-30 16:39:46 +0200

    fixed unit test running

--- rollup.test-integration-config.js
+++ rollup.test-integration-config.js
@@ -4,9 +4,10 @@ import clear from "rollup-plugin-clear";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import buble      from 'rollup-plugin-buble';
+import buble from 'rollup-plugin-buble';
 import multiEntry from 'rollup-plugin-multi-entry';
 import nodent from 'rollup-plugin-nodent';
+import replace from 'rollup-plugin-replace';
 
 export default {
   input: 'test/integration/**/*.test.ts',
@@ -23,10 +24,18 @@ export default {
   },
   external: ['chai', 'it', 'describe'],
   plugins: [
+    replace({
+      // returns 'true' if code is bundled in prod mode
+      // PRODUCTION: JSON.stringify(isProduction),
+      // you can also use this to include deploy-related data, such as
+      // date + time of build, as well as latest commit ID from git
+      __BUILD_TIME__: JSON.stringify(Date.now()),
+      __REVISION__: JSON.stringify(require('git-rev-sync').short()),
+    }),
     clear({ targets: ["dist/test.bundle.js"] }),
     resolve(),
     commonjs(),
-    typescript({tsconfig: "./tsconfig.test-integration.json"}),
+    typescript({ tsconfig: "./tsconfig.test-integration.json" }),
     nodent(),
     multiEntry(),
     buble()
--- rollup.test-unit-config.js
+++ rollup.test-unit-config.js
@@ -4,8 +4,9 @@ import clear from "rollup-plugin-clear";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import buble      from 'rollup-plugin-buble';
+import buble from 'rollup-plugin-buble';
 import multiEntry from 'rollup-plugin-multi-entry';
+import replace from 'rollup-plugin-replace';
 
 export default {
   input: 'test/unit/**/*.test.ts',
@@ -22,11 +23,19 @@ export default {
   },
   external: ['chai', 'it', 'describe'],
   plugins: [
+    replace({
+      // returns 'true' if code is bundled in prod mode
+      // PRODUCTION: JSON.stringify(isProduction),
+      // you can also use this to include deploy-related data, such as
+      // date + time of build, as well as latest commit ID from git
+      __BUILD_TIME__: JSON.stringify(Date.now()),
+      __REVISION__: JSON.stringify(require('git-rev-sync').short()),
+    }),
     clear({ targets: ["dist/test.bundle.js"] }),
     resolve(),
     commonjs(),
-    typescript({tsconfig: "./tsconfig.json"}),
-    multiEntry(),
-    buble()
+    typescript({ tsconfig: "./tsconfig.json" }),
+    multiEntry()
+    // buble()
   ]
 }
