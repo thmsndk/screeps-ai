1fb442dc778fe05dc652ba4f9d017a80d351f2f7
thmsn <thmsn.dk@gmail.com>
2019-12-21 02:56:34 +0100

    make claimers navigate to rooms with no vision

--- src/missions/ClaimMission.ts
+++ src/missions/ClaimMission.ts
@@ -44,7 +44,7 @@ export class ClaimMission extends Mission {
       count: neededWorkers - (this.memory.creeps.claimers.length || 0),
       // 650 energy https://screeps.arcath.net/creep-designer/?share=1#0#0#0#0#0#1#0
       runePowers: { [MOVE]: 1, [CLAIM]: 1 },
-      priority: 1, // TODO: change priorty perhaps it should be a tab-step?
+      priority: 2, // TODO: change priorty perhaps it should be a tab-step?
       mission: this.memory.id
     }
 
@@ -63,34 +63,34 @@ export class ClaimMission extends Mission {
       // TODO: Assign tasks
       const claimer = idleclaimers.pop() // TODO: We should pick the closest creep not just any idle creep
 
-      const controller = this.room?.controller
-      if (controller) {
-        const neutralController = !controller.my && controller.level === 0
+      if (claimer) {
+        if (!this.goToRoom(claimer, this.roomName)) {
+          const controller = this.room?.controller
+          if (controller) {
+            const neutralController = !controller.my && controller.level === 0
 
-        if (claimer) {
-          if (!this.goToRoom(claimer, this.roomName)) {
             // // log.info(`neutral: ${neutralController} level: ${controller.level}`)
             if (!claimer.task && neutralController) {
               claimer.task = Tasks.claim(controller)
             }
-          }
-        }
 
-        if (!neutralController && controller.my && !this.roomMemory.settlement && !this.roomMemory.village) {
-          this.roomMemory.runPlanner = true
-          this.roomMemory.settlement = true
-          this.roomMemory.outpost = false
-          this.roomMemory.claim = false
-        }
-        if (this.roomMemory.claim && controller.my) {
-          this.roomMemory.claim = false
-          const claimFlags = this.room?.find(FIND_FLAGS, {
-            filter: flag =>
-              flag.name.startsWith("claim") || flag.name.startsWith("remote") || flag.name.startsWith("reserve")
-          })
+            if (!neutralController && controller.my && !this.roomMemory.settlement && !this.roomMemory.village) {
+              this.roomMemory.runPlanner = true
+              this.roomMemory.settlement = true
+              this.roomMemory.outpost = false
+              this.roomMemory.claim = false
+            }
+            if (this.roomMemory.claim && controller.my) {
+              this.roomMemory.claim = false
+              const claimFlags = this.room?.find(FIND_FLAGS, {
+                filter: flag =>
+                  flag.name.startsWith("claim") || flag.name.startsWith("remote") || flag.name.startsWith("reserve")
+              })
 
-          if (claimFlags) {
-            claimFlags.forEach(flag => flag.remove())
+              if (claimFlags) {
+                claimFlags.forEach(flag => flag.remove())
+              }
+            }
           }
         }
       }
